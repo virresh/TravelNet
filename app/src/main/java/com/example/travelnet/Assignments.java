1e053558b8c4667509d8f71c0ae9fd2863ee7150
Viresh Gupta <viresh16118@iiitd.ac.in>
2019-08-26 00:11:34 +0530

    Add all Activities
    Add all four activities
    Two of them also have a layout customized

---
+++ app/src/main/java/com/example/travelnet/Assignments.java
@@ -0,0 +1,14 @@
+package com.example.travelnet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Assignments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignments);
+    }
+}
